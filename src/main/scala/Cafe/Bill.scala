0dd2f97da8511cafd08f52bac93fe7c701e05055
Ashley George <ashgeo1986@gmail.com>
2025-03-05 15:53:09 +0000

    added service

--- src/main/scala/Cafe/Bill.scala
+++ src/main/scala/Cafe/Bill.scala
@@ -5,7 +5,9 @@ class Bill(val customerChoices: List[MenuItems], val serviceCharge: Option[Doubl
 
   //helper functions to calculate service charge
   def tenPercent: Double => Double = price => price * 0.1
+
   def twentyPercent: Double => Double = price => price * 0.2
+
   def twentyFivePercent: Double => Double = price => price * 0.25
 
   //calculate total of the bill (with and without service charge)
@@ -15,16 +17,14 @@ class Bill(val customerChoices: List[MenuItems], val serviceCharge: Option[Doubl
 
 
     //calculate service charge based on food type - No idea how to finish this!
-    serviceCharge match {
-      case Some(charge) => total + charge
-      case None =>
-        // Calculate the service charge based on food type
-        val serviceChargeAmount = customerChoices.map {
-          case MenuItems(ColdFood) => tenPercent
-          case MenuItems(HotFood) => twentyPercent
-          case MenuItems(Drink) => 0.0
-        }.sum
-    }
+    def AddServiceCharge(customerChoices: List[MenuItems], serviceCharge: Option[Double] = None): Double = {
+        Bill match {
+          case service if Drink => 0.0
+          case service if ColdFood => tenPercent
+          case service if HotFood => twentyPercent
+          case service if
+          case _ => 0.0
+        }
+      }
   }
 }
-
